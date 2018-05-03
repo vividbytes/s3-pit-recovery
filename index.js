c245b70d6d4890b45702286143308e9036d0af4b
vividbytes <lauri@vividbytes.io>
2018-05-03 18:09:38 +0300

    add parameter validation and Glacier restoration

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const {promisify} = require('util');
 
 const mkdirAsync = promisify(fs.mkdir);
 
-s3PitRecovery()
+s3PitRecovery({
+    bucket: 'notes.vividbytes.io',
+    destination: 'test'
+})
     .catch(e => console.error(e.message));
 
-const Bucket = 'notes.vividbytes.io';
-
-async function getObjectsToRecover(timestamp) {
+async function getObjectsToRecover(options) {
     let isMore = true;
     let nextKey; 
     const objects = {};
@@ -20,8 +21,8 @@ async function getObjectsToRecover(timestamp) {
 
     while(isMore) {
 	let data = await S3.listObjectVersions({
-	    Bucket: Bucket,
-	    Prefix: '',
+	    Bucket: options.bucket,
+	    Prefix: options.prefix,
 	    KeyMarker: nextKey,
 	    MaxKeys: 6
 	}).promise();
@@ -30,10 +31,10 @@ async function getObjectsToRecover(timestamp) {
 	    const currObj = deleteMarkers[obj.Key];
 	    const lastModified = new Date(obj.LastModified);
 
-	    if (!currObj && obj.LastModified <= timestamp) {
+	    if (!currObj && obj.LastModified <= options.time) {
 		deleteMarkers[obj.Key] = obj;
 	    } else if (currObj &&
-		       lastModified <= timestamp &&
+		       lastModified <= options.time &&
 		       lastModified > new Date(currObj.LastModified)
 		      ) {
 		deleteMarkers[obj.Key] = obj;
@@ -44,10 +45,10 @@ async function getObjectsToRecover(timestamp) {
 	    const currObj = objects[obj.Key];
 	    const lastModified = new Date(obj.LastModified);
 
-	    if (!currObj && obj.LastModified <= timestamp) {
+	    if (!currObj && obj.LastModified <= options.time) {
 		objects[obj.Key] = obj;
 	    } else if (currObj &&
-		       lastModified <= timestamp &&
+		       lastModified <= options.time &&
 		       lastModified > new Date(currObj.LastModified)
 		      ) {
 		objects[obj.Key] = obj;
@@ -74,36 +75,95 @@ async function getObjectsToRecover(timestamp) {
     return objects;
 }
 
-async function requestObjectRecovery(obj) {
-    console.log('recovering object', obj.Key);
-    console.log('lastModified', obj.LastModified);
+async function requestObjectRecovery(obj, options) {
+    console.log('restoring object from Glacier', obj.Key);
+
+    return S3.restoreObject({
+	Bucket: options.bucket,
+	Key: obj.Key,
+	VersionId: obj.VersionId,
+	RestoreRequest: {
+	    Days: options.glacierDays,
+	    GlacierJobParameters: {
+		Tier: options.glacierTier
+	    }
+	}
+    }).promise();
 }
 
-async function retrieveObject(obj, dir) {
-    console.log('retrievingObjectFromS3', obj.Key);
-    console.log('lastModified', obj.LastModified);
+async function retrieveObject(obj, options) {
+    console.log('retrieving object from S3', obj.Key);
 
     return new Promise((resolve, reject) => {
 	const pipe = S3.getObject({
-	    Bucket: Bucket,
-	    Key: obj.Key
+	    Bucket: options.bucket,
+	    Key: obj.Key,
+	    VersionId: obj.VersionId
 	})
 	.createReadStream()
-	.pipe(fs.createWriteStream(path.resolve(dir, obj.Key)))
+	.pipe(fs.createWriteStream(path.resolve(options.destination, obj.Key)))
 	.on('finish', resolve)
 	.on('error', reject);
     });
 }
 
-async function s3PitRecovery(dir = 'test') {
-    await mkdirAsync(dir);
-    const objects =  await getObjectsToRecover(new Date('2018-04-30'));
+
+async function s3PitRecovery(options = {}) {
+    validateConfig(options);
+
+    const defaultOptions = {
+	time: new Date(),
+	prefix: '',
+	glacierTier: 'Standard',
+	glacierDays: 3
+    };
+
+    const config = Object.assign({}, defaultOptions, options);
+    config.time = new Date(config.time);
+
+
+    await mkdirAsync(options.destination);
+
+    const objects =  await getObjectsToRecover(config);
 
     return Promise.map(Object.keys(objects).map(key => objects[key]), obj => {
     	if (obj.StorageClass === 'GLACIER') {
-	    return requestObjectRecovery(obj);
+	    return requestObjectRecovery(obj, config);
     	} else {
-    	    return retrieveObject(obj, dir);
+    	    return retrieveObject(obj, config);
     	}
-    }, { concurrency: 10 });
+    }, { concurrency: 1 });
+}
+
+function validateConfig(config) {
+
+    if (!config.destination) {
+	throw new TypeError('parameter --destination is required');
+    }
+
+    if (!config.bucket) {
+	throw new TypeError('parameter --bucket is required');
+    }
+
+    if (
+	config.clacierTier &&
+	!['Standard', 'Expedited', 'Bulk'].includes(config.glacierTier)) {
+	throw new TypeError('parameter --glacierTier must be one of Standard, Expedited, Bulk');
+    }
+
+    if (
+	    config.glacierDays &&
+	    /^0+$/.test(config.glacierDays) &&
+	    !/^[0-9]+$/.test(config.glacierDays)
+       ) {
+	throw new TypeError('parameter --glacierDays must be a positive integer');
+    }
+
+    if (
+	config.time &&
+	new Date(config.time).toString() === 'Invalid Date'
+    ) {
+	throw new TypeError('parameter --time must be a valid JSON string');
+    }
+
 }
